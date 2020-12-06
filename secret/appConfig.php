9d8e3a170d1fa60af4b676cd8b56444b36de91ac
NhanNgocThien <nhanthien1012@gmail.com>
2020-12-06 22:36:50 +0700

    Reset db config

--- secret/appConfig.php
+++ secret/appConfig.php
@@ -3,10 +3,7 @@
 
         //Database configuration
         'configDB' => array (
-            // 'servername' => "localhost",
-            // 'username' => "root",
-            // 'password' => "abcdef1234",
-            // 'dbname' => "db_ass2_test",
+            'servername' => "localhost",
             'username' => "manager",
             'password' => "tien1234",
             'dbname' => "fabric",
