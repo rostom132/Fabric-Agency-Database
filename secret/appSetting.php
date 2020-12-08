49c33a5c6bf27caf66433e486988170ffe16c54c
NhanNgocThien <nhanthien1012@gmail.com>
2020-12-08 11:57:08 +0700

    Update UI search supplier + Add error response

--- secret/appSetting.php
+++ secret/appSetting.php
@@ -19,10 +19,14 @@
         // validation every data insert into database 
         'validation' => array(
             'supplier' => array (
-                'name' => '/^[a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s|_]+$/',
-                'address' => '/^[\/a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s|_]+$/',
+                'name' => '/^[a-zA-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s]+$/',
+                'name_error_response' => 'Name only contains letters',
+                'address' => '/^[\/a-zA-Z0-9ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂẾưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ\s|_,]+$/',
+                'address_error_response' => 'Address only contain letters and characters like |_,',
                 'bank_account' => '/^\w{5,30}$/',
-                'tax' => '/^\w{5,30}/'
+                'bank_account_error_response' => 'Bank account has minimum of 5 digtis and maxium of 30 digits',
+                'tax' => '/^\w{5,30}/',
+                'tax_error_response' => 'Tax code has minimum of 5 digits and maxium of 30 digits'
             ),
             'supplier_phonenumber' => array (
                 'phone' => '/^\d{10}$/',
